6e723f7fa3552ffd63af4d59b8a6297636556387
Maxence Souchon <maxence.souchon@insa-lyon.fr>
2016-04-27 00:26:23 +0200

    FAB is now at the right position + using FAB library from Google instead of com.github.markushi:circlebutton

--- app/src/main/java/com/h4112/androidparking/MainActivity.java
+++ app/src/main/java/com/h4112/androidparking/MainActivity.java
@@ -7,6 +7,7 @@ import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -17,6 +18,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.Toast;
@@ -80,6 +82,20 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         initPlayServices();
         initDrawer();
         initDrawerList();
+
+        FloatingActionButton navigate = (FloatingActionButton) findViewById(R.id.boutonItineraire);
+        if (navigate != null) {
+            navigate.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    navigationButtonClicked();
+                }
+            });
+        }
+    }
+
+    private void navigationButtonClicked() {
+        Log.i("MainActivity", "TODO: navigate!");
     }
 
     /**
